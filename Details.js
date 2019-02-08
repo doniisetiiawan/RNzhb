97466e726eb68faec1bea842d210e7dc0d42a4c4
Doni Setiawan <donisetiawan.050@gmail.com>
2019-02-08 12:24:52 +0700

    Add navigation header

--- Details.js
+++ Details.js
@@ -1,11 +1,28 @@
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 import styles from './styles';
 
-export default ({ navigation }) => (
+const Details = ({ navigation }) => (
   <View style={styles.container}>
-    <Text>{navigation.getParam('title')}</Text>
+    <Text>{navigation.getParam('content')}</Text>
   </View>
 );
+
+Details.propTypes = {
+  navigation: PropTypes.any.isRequired,
+};
+
+Details.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('title'),
+  headerRight: (
+    <Button
+      title="Buy"
+      onPress={() => {}}
+      disabled={navigation.getParam('stock') === 0}
+    />
+  ),
+});
+
+export default Details;
--- Home.js
+++ Home.js
@@ -1,23 +1,45 @@
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 import styles from './styles';
 
-export default ({ navigation }) => (
+const Home = ({ navigation }) => (
   <View style={styles.container}>
     <Text>Home Screen</Text>
     <Button
       title="First Item"
-      onPress={() => navigation.navigate('Details', { title: 'First Item' })}
+      onPress={() => navigation.navigate('Details', {
+        title: 'First Item',
+        content: 'First Item Content',
+        stock: 1,
+      })}
     />
     <Button
       title="Second Item"
-      onPress={() => navigation.navigate('Details', { title: 'Second Item' })}
+      onPress={() => navigation.navigate('Details', {
+        title: 'Second Item',
+        content: 'Second Item Content',
+        stock: 0,
+      })}
     />
     <Button
       title="Third Item"
-      onPress={() => navigation.navigate('Details', { title: 'Third Item' })}
+      onPress={() => navigation.navigate('Details', {
+        title: 'Third Item',
+        content: 'Third Item Content',
+        stock: 200,
+      })}
     />
   </View>
 );
+
+Home.propTypes = {
+  navigation: PropTypes.any.isRequired,
+};
+
+Home.navigationOptions = {
+  title: 'Home',
+};
+
+export default Home;
