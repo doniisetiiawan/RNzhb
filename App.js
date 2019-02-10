deb9a18c60b4e3caf8eca0b3bb76a69d6433df36
Doni Setiawan <donisetiawan.050@gmail.com>
2019-02-10 16:08:39 +0700

    Add plotting points

--- App.js
+++ App.js
@@ -8,8 +8,26 @@ export default () => (
   <View style={styles.container}>
     <MapView
       style={styles.mapView}
+      showsPointsOfInterest={false}
       showsUserLocation
       followUserLocation
-    />
+    >
+      <MapView.Marker
+        title="Duff Brewery"
+        description="Duff beer for me, Duff beer for you"
+        coordinate={{
+          latitude: 43.8418728,
+          longitude: -79.086082,
+        }}
+      />
+      <MapView.Marker
+        title="Pawtucket Brewery"
+        description="New! Patriot Light!"
+        coordinate={{
+          latitude: 43.8401328,
+          longitude: -79.085407,
+        }}
+      />
+    </MapView>
   </View>
 );
